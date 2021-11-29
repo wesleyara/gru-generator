cc5807a1ec38f1e9ee2871a60fac9d5ee7f871ac
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2021-11-29 18:14:45 -0300

    finalizando a versão 1.0.0

--- index.js
+++ index.js
@@ -5,33 +5,35 @@ var readlineSync = require('readline-sync');
 const replaceAll = require('string.prototype.replaceall');
 replaceAll.shim();
 
-// var unidadeGestora = parseInt(readlineSync.question(`Unidade Gestora: `)) 153098
+var unidadeGestora = parseInt(readlineSync.question(`Unidade Gestora: `)) 
 var vencimento = parseInt(readlineSync.question(`Para que data o vencimento? (ex: ddmmaaaa): `))
 var cpf = parseInt(readlineSync.question(`Digite seu CPF (somente numeros): `))
 var nomeContribuinte = readlineSync.question(`Digite o nome do contribuinte: `)
+var valorPrincipal = readlineSync.question(`Digite o valor principal e total (ex: 5000 para R$50,00): `)
+
 
 async function GRUbot() {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto('http://consulta.tesouro.fazenda.gov.br/gru_novosite/gru_simples.asp');
 
-    await page.waitForTimeout(3000)
+    await page.waitForTimeout(5000)
     await page.focus('#codigo_favorecido')
     await page.waitForTimeout(1000)
-    page.keyboard.type(`153098`)
+    page.keyboard.type(`${unidadeGestora}`)
     await page.waitForTimeout(1000)
     const inputElement = await page.$('#nome_favorecido');
     await inputElement.click();
 
-    await page.waitForTimeout(2000)
+    await page.waitForTimeout(1000)
 
     await page.select('#gestao', '15233**7788')
 
-    await page.waitForTimeout(2000)
+    await page.waitForTimeout(1000)
 
     await page.select('#codigo_recolhimento', '28832-2')
 
-    await page.waitForTimeout(2000)
+    await page.waitForTimeout(1000)
 
     const inputElement1 = await page.$('#image1');
     await inputElement1.click();
@@ -60,9 +62,28 @@ async function GRUbot() {
     await page.waitForTimeout(1000)
     page.keyboard.type(`${nomeContribuinte}`)
 
-    await page.waitForTimeout(10000)
+    await page.waitForTimeout(1000)
 
+    await page.focus('#valorPrincipal')
+    await page.waitForTimeout(1000)
+    page.keyboard.type(`${valorPrincipal}`)
 
+    await page.waitForTimeout(1000)
+
+    await page.focus('#valorTotal')
+    await page.waitForTimeout(1000)
+    page.keyboard.type(`${valorPrincipal}`)
+
+    await page.waitForTimeout(1000)
+
+    await page.select('#boleto', '4')
+    
+    await page.waitForTimeout(1000)
+
+    const inputElement3 = await page.$('#image1');
+    await inputElement3.click();
+
+    await page.waitForTimeout(5000)
 
     await browser.close();
 }
